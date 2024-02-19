de0a3747bb56ad8e5badb2073fd0515accdee1b7
msanchezac <msanchezac@unal.edu.co>
2024-02-19 17:53:23 -0500

    Correción 1

--- main.py
+++ main.py
@@ -9,26 +9,26 @@ class Auto:
       self.registro = registro
       Auto.cantidadCreados +=1
 
-def cantidadAsientos(self):
-   c = 0
-   for asiento in self.asientos:
+   def cantidadAsientos(self):
+     c = 0
+     for asiento in self.asientos:
         if isinstance(asiento, Asiento):
             c += 1
-   return c
+     return c
    
-def verificarIntegridad(self):
-   registro_auto = self.registro
-   registro_motor = self.motor.registro if self.motor else None
-   asientos_originales = True
-    
-   for asiento in self.asientos:
-        if isinstance(asiento, Asiento) and asiento.registro != registro_auto:
-            asientos_originales = False
-            break
+   def verificarIntegridad(self):
+      registro_auto = self.registro
+      registro_motor = self.motor.registro if self.motor else None
+      asientos_originales = True
+      
+      for asiento in self.asientos:
+       if isinstance(asiento, Asiento) and asiento.registro != registro_auto:
+        asientos_originales = False
+        break
 
-   if registro_motor is not None and registro_auto == registro_motor and asientos_originales:
+      if registro_motor is not None and registro_auto == registro_motor and asientos_originales:
         return "Auto original"
-   else:
+      else:
         return "Las piezas no son originales"
 
 class Asiento:
@@ -37,7 +37,7 @@ class Asiento:
       self.precio = precio
       self.registro = registro
 
-def cambiarColor(self, nuevoColor):
+   def cambiarColor(self, nuevoColor):
         colores = ["rojo", "verde", "amarillo", "negro", "blanco"]
         if nuevoColor in colores:
             self.color = nuevoColor
@@ -48,11 +48,11 @@ class Motor:
       self.tipo = tipo
       self.registro = registro
 
-def cambiarRegistro(self, nuevoRegistro):
-        self.registro = nuevoRegistro
+   def cambiarRegistro(self, nuevoRegistro):
+      self.registro = nuevoRegistro
 
-def asignarTipo(self, nuevoTipo):
-        tipos = ["electrico", "gasolina"]
-        if nuevoTipo in tipos:
-            self.tipo = nuevoTipo
+   def asignarTipo(self, nuevoTipo):
+      tipos = ["electrico", "gasolina"]
+      if nuevoTipo in tipos:
+       self.tipo = nuevoTipo
 
